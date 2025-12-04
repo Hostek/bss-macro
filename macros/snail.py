3bdd593a096e2803d837299c8d0dfec914ee17c0
Hostek <michalpl245674477@gmail.com>
2025-12-04 18:14:45 +0100

    upd snail.py

--- macros/snail.py
+++ macros/snail.py
@@ -9,8 +9,7 @@ def run():
     while True:
         print("   -> Jump/Gliding...")
         # Hold space for 3 seconds
-        core.hold_key("space", 1.5)
-        core.hold_key("space", 1.5)
+        core.hold_key("space", 3)
 
         print("   -> Waiting 1 minutes...")
         # Wait 1 minutes (60 seconds)
